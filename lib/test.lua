4d49278bea74eecd668b7595e3438b2d04f20608
jack.zh <zzh.coder@gmail.com>
2014-11-19 11:44:53 +0800

    init source

---
+++ lib/test.lua
@@ -0,0 +1,13 @@
+function somaP (x1,y1,x2,y2)
+  return x1+x2, y1+y2
+end
+
+function norma (x,y)
+  return x*x+y*y
+end
+
+function retorno_multiplo ()
+ print (norma(somaP(2,3,4,5)))
+end
+
+print (somaP(1,2,3,4))
